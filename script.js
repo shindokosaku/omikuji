bd467c17e3830d08e2d704f00ce7ca4dab23e80d
EC2 Default User <ec2-user@ip-172-31-43-29.eu-north-1.compute.internal>
2025-02-24 03:31:52 +0000

    仮完成

---
+++ script.js
@@ -0,0 +1,36 @@
+(() => {
+    const DURATION = 3000;
+    const SLOT_TIME = 100;
+    const omikujiArray = [
+        "./images/_omikuji_daikichi.png",
+        "./images/_omikuji_chuutichi.png",
+        "./images/_omikuji_syoukichi.png",
+        "./images/_omikuji_suekichi.png",
+        "./images/_omikuji_kichi.png",
+        "./images/_omikuji_kyou.png",
+        "./images/_omikuji_daikyou.png"
+    ];
+    const $result = document.getElementById("js-result");
+    //先頭に$をつけるのは変数がHTML要素を格納していることを明示するためのプレフィックス;
+    // プレフィックスとはクラス名や変数名の冒頭になんらかの文字列を付与すること;
+    const getRandomNumber = (argMin, argMax) => {
+      const min = Math.ceil(argMin);
+      const max = Math.floor(argMax);
+      return Math. floor(Math.random() * (max-min) + min);
+    }
+
+    const playomikuji = () => {
+        const slotAnimationTimer = setInterval(() => {
+          const randomkey = getRandomNumber(0,6);
+          $result.setAttribute("src", omikujiArray[randomKey]);
+        },SLOT_TIME);
+        
+        setTimeout(() => {
+          clearInterval(slotAnimationTimer);
+          const randomKey = getRandomNumber(0,6);
+          $result.setAttribute("src", omikujiArray[randomKey]);
+        }, DURATION);
+    };
+
+    document.getElementById("js-button").addEventListener("click",playomikuji);
+    })();
